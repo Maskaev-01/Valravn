1f2d4da63951407e793c6db991a5149b9418fa7d
v_maskaev <maskaev-01@mail.ru>
2025-07-13 21:40:14 +0400

    Тестим роли

--- app/routers/admin.py
+++ app/routers/admin.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, Request, Form, HTTPException
+from fastapi import APIRouter, Depends, Request, Form, HTTPException, Query
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
 from sqlalchemy.orm import Session
